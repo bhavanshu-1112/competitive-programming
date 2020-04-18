d5e6724f8f17d62a2140a6b98aacfaaa0bf6573e
Bhavanshu Jain <46085014+bhavanshu-1112@users.noreply.github.com>
2020-04-18 11:43:41 +0530

    Create dp2.cpp

---
+++ dp2.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+// bottom up approach
+
+int fibBU(int n){
+    int dp[100] = {0};
+    dp[1] =1;
+    for(int i=2;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+
+    }
+return dp[n];
+}
+int main() {
+  int n;
+  cin>>n;
+  cout<<fibBU(n)<<endl;
+}
